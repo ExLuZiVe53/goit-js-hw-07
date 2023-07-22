84a38b8f56b79a6ff10a09557b0a83893fc912bb
ExLuZiVe53 <imstudent27@gmail.com>
2023-07-22 22:24:23 +0200

    update task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+galleryEl.innerHTML("beforeend", createGallery(galleryItems));
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+function createGallery(galleryItems) {
+  return galleryItems.map(({ descripion, preview, original }) => {
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${descripion}" />
+        </a>
+    </li>`;
+  });
+}
